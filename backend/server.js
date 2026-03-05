df7883077669ffb95da74bfabbb5b2ea57d9bb6b
Your Name <you@example.com>
2026-03-05 15:20:26 +0530

    Update before repush

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,9 @@ const app = express();
 // Middleware
 app.use(helmet());
 app.use(cors({
-    origin: ['http://localhost:8080', 'http://localhost:5173', 'http://127.0.0.1:8080', 'http://127.0.0.1:5173'],
+    origin: function (origin, callback) {
+        callback(null, true);
+    },
     credentials: true
 }));
 app.use(morgan('dev'));
